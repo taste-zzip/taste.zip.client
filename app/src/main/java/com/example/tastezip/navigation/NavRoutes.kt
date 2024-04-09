5d400ec845c9b2cd7bec56d14217c3de4fc49670
dogmania <hj296914@gmail.com>
2024-04-09 16:52:29 +0900

    :sparkles: Feat: UserInfo 경로 추가

--- app/src/main/java/com/example/tastezip/navigation/NavRoutes.kt
+++ app/src/main/java/com/example/tastezip/navigation/NavRoutes.kt
@@ -3,4 +3,5 @@ package com.example.tastezip.navigation
 sealed class NavRoutes(val route: String) {
     data object Splash: NavRoutes("splash")
     data object Login: NavRoutes("login")
+    data object UserInfo: NavRoutes("userInfo")
 }
