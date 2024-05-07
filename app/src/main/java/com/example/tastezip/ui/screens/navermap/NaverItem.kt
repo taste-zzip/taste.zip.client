70bd50668990d7ceb500fc6b59635d56f01f8187
dogmania <hj296914@gmail.com>
2024-05-07 14:53:07 +0900

    :sparkles: Feat: 클러스터링 구현을 위한 아이템 구현

---
+++ app/src/main/java/com/example/tastezip/ui/screens/navermap/NaverItem.kt
@@ -0,0 +1,19 @@
+package com.example.tastezip.ui.screens.navermap
+
+import com.naver.maps.geometry.LatLng
+import ted.gun0912.clustering.clustering.TedClusterItem
+import ted.gun0912.clustering.geometry.TedLatLng
+
+data class NaverItem(var position: LatLng): TedClusterItem {
+    override fun getTedLatLng(): TedLatLng {
+        return TedLatLng(position.latitude, position.longitude)
+    }
+
+    var title: String? = null
+    var snippet: String? = null
+
+    constructor(lat: Double, lng: Double) : this(LatLng(lat, lng)) {
+        title = null
+        snippet = null
+    }
+}
