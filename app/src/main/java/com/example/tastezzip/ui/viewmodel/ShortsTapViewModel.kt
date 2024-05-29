920af10b688f96089498fcdec2623e7e7d7a7c7c
dogmania <hj296914@gmail.com>
2024-05-29 23:47:25 +0900

    :sparkles: Feat: 하단 네비게이션바 쇼츠 탭 페이지 구현

---
+++ app/src/main/java/com/example/tastezzip/ui/viewmodel/ShortsTapViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.tastezzip.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tastezzip.data.repository.VideoRepository
+import com.example.tastezzip.model.response.shorts.Feed
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ShortsTapViewModel @Inject constructor(
+    private val videoRepository: VideoRepository
+) : ViewModel() {
+    private val _videoList: MutableStateFlow<List<Feed>> = MutableStateFlow(emptyList())
+    val videoList = _videoList.asStateFlow()
+
+    init {
+        getFeedList()
+    }
+
+    fun getFeedList() {
+        viewModelScope.launch {
+            val response = videoRepository.getFeedList(20)
+            _videoList.update { response.feedList }
+        }
+    }
+}
