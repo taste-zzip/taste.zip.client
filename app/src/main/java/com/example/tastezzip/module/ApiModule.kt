1e8c632c5a510bfd12218b9738d26a71a8d66b78
dogmania <hj296914@gmail.com>
2024-05-29 23:46:21 +0900

    :sparkles: Feat: Hilt 모듈 구현

---
+++ app/src/main/java/com/example/tastezzip/module/ApiModule.kt
@@ -0,0 +1,49 @@
+package com.example.tastezzip.module
+
+import com.example.tastezzip.data.api.AuthApi
+import com.example.tastezzip.data.api.CafeteriaApi
+import com.example.tastezzip.data.api.VideoApi
+import com.example.tastezzip.data.api.YoutubeApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApiModule {
+    @Singleton
+    @Provides
+    fun provideAuthApi(
+        retrofit: Retrofit
+    ): AuthApi {
+        return retrofit.create(AuthApi::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideCafeteriaApi(
+        retrofit: Retrofit
+    ): CafeteriaApi {
+        return retrofit.create(CafeteriaApi::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideVideoApi(
+        retrofit: Retrofit
+    ): VideoApi {
+        return retrofit.create(VideoApi::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideYoutubeApi(
+        retrofit: Retrofit
+    ): YoutubeApi {
+        return retrofit.create(YoutubeApi::class.java)
+    }
+}
