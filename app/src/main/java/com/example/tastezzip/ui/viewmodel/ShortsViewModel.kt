536d711be342027a303cfafde647b1b5ab644eb9
dogmania <hj296914@gmail.com>
2024-05-29 23:34:29 +0900

    :sparkles: Feat: 쇼츠 좋아요 기능 구현, 임시 데이터 삭제

--- app/src/main/java/com/example/tastezzip/ui/viewmodel/ShortsViewModel.kt
+++ app/src/main/java/com/example/tastezzip/ui/viewmodel/ShortsViewModel.kt
@@ -1,70 +1,56 @@
-package com.example.tastezip.ui.viewmodel
+package com.example.tastezzip.ui.viewmodel
 
+import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.pager.PagerState
-import androidx.compose.runtime.MutableState
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.tastezzip.data.repository.CafeteriaRepository
+import com.example.tastezzip.data.repository.VideoRepository
+import com.example.tastezzip.model.enums.LikeType
+import com.example.tastezzip.model.request.AddLikeVideoRequestVo
+import com.example.tastezzip.model.request.BookmarkCafeteriaRequestVo
+import com.example.tastezzip.model.request.DeleteLikeVideoRequestVo
+import com.example.tastezzip.repository.VideoRepositoryImpl
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
-
-data class VideoItem(
-    val videoId: String,
-    val title: String,
-    val thumbnail: String,
-    val id: Long
-)
 
 @HiltViewModel
 @OptIn(ExperimentalFoundationApi::class)
-class ShortsViewModel @Inject constructor() : ViewModel() {
-    val videoList = listOf(
-        VideoItem(
-            "R1efJCVdaFU",
-            "래퍼들은 가사를 어떻게 쓸까?",
-            "https://thumb.mt.co.kr/06/2024/03/2024030110307220960_4.jpg",
-            1
-        ),
-        VideoItem(
-            "uGTE7wnGMbg",
-            "래퍼들은 가사를 어떻게 쓸까?",
-            "https://thumb.mt.co.kr/06/2024/03/2024030110307220960_4.jpg",
-            2
-        ),
-        VideoItem(
-            "ZpMvn9NivMU",
-            "래퍼들은 가사를 어떻게 쓸까?",
-            "https://thumb.mt.co.kr/06/2024/03/2024030110307220960_4.jpg",
-            3
-        ),
-        VideoItem(
-            "0g42vsNH-wA",
-            "래퍼들은 가사를 어떻게 쓸까?",
-            "https://thumb.mt.co.kr/06/2024/03/2024030110307220960_4.jpg",
-            4
-        ),
-        VideoItem(
-            "L3brB2rpXh0",
-            "래퍼들은 가사를 어떻게 쓸까?",
-            "https://thumb.mt.co.kr/06/2024/03/2024030110307220960_4.jpg",
-            5
-        )
-    )
-    private val _pageStateFlow: MutableStateFlow<PagerState> = MutableStateFlow(PagerState(currentPage = 0, currentPageOffsetFraction = 0.0f) { videoList.size })
-    private val _currentPageIdx: MutableStateFlow<Int> = MutableStateFlow(0)
-    private val _currentVideoItem: MutableStateFlow<VideoItem> = MutableStateFlow(videoList[0])
+class ShortsViewModel @Inject constructor(
+    private val videoRepository: VideoRepository
+) : ViewModel() {
+    private val _pageStateFlow: MutableStateFlow<PagerState> = MutableStateFlow(PagerState(currentPage = 0, currentPageOffsetFraction = 0.0f) { 0 })
+    private val _likeStateFlow: MutableStateFlow<Boolean> = MutableStateFlow(false)
     val pageStateFlow = _pageStateFlow.asStateFlow()
-    val currentPageIdx = _currentPageIdx.asStateFlow()
-    val currentVideoItem = _currentVideoItem.asStateFlow()
+    val likeState: StateFlow<Boolean> = _likeStateFlow.asStateFlow()
 
-    fun updateCurrentPageIdx(index: Int) {
-        _currentPageIdx.update { index }
+    fun updatePageCount(videoSize: Int) {
+        viewModelScope.launch(Dispatchers.Main) {
+            _pageStateFlow.update { PagerState(0, 0F) { videoSize } }
+            Log.e("anjrk answpdi", _pageStateFlow.value.pageCount.toString())
+        }
     }
 
-    fun updateCurrentItem(index: Int) {
-        _currentVideoItem.update { videoList[index] }
+    fun updateLikeState(current: LikeType?, videoId: Long) {
+        viewModelScope.launch(Dispatchers.Main) {
+            if (current == LikeType.LIKE) {
+                videoRepository.deleteLikeVideo(videoId)
+                _likeStateFlow.update { false }
+            } else {
+                videoRepository.addLikeVideo(AddLikeVideoRequestVo(videoId))
+                _likeStateFlow.update { true }
+            }
+
+        }
     }
+
 }
 
