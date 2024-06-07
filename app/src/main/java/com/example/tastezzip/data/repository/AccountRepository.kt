29e5e14a491708e436f3c1f1738cdec61e21428d
dogmania <hj296914@gmail.com>
2024-06-08 02:34:36 +0900

    :sparkles: Feat: 마이페이지 api 연결

--- app/src/main/java/com/example/tastezzip/data/repository/AccountRepository.kt
+++ app/src/main/java/com/example/tastezzip/data/repository/AccountRepository.kt
@@ -1,5 +1,8 @@
 package com.example.tastezzip.data.repository
 
+import com.example.tastezzip.model.response.account.AccountResponse
+
 interface AccountRepository {
+    suspend fun getAccount(): AccountResponse
     suspend fun deleteAccount()
 }
--- app/src/main/java/com/example/tastezzip/data/repositoryImpl/AccountRepositoryImpl.kt
+++ app/src/main/java/com/example/tastezzip/data/repositoryImpl/AccountRepositoryImpl.kt
@@ -2,11 +2,15 @@ package com.example.tastezzip.data.repositoryImpl
 
 import com.example.tastezzip.data.api.AccountApi
 import com.example.tastezzip.data.repository.AccountRepository
+import com.example.tastezzip.model.response.account.AccountResponse
 import javax.inject.Inject
 
 class AccountRepositoryImpl @Inject constructor(
     private val accountApi: AccountApi
 ) : AccountRepository {
+    override suspend fun getAccount(): AccountResponse {
+        return accountApi.getAccount()
+    }
     override suspend fun deleteAccount() {
         return accountApi.deleteAccount()
     }
