fa370f01c75b21f3b3b239572949d504b9dbe074
dogmania <hj296914@gmail.com>
2024-06-09 01:48:37 +0900

    :sparkles: Feat: 댓글 생성 api 연결

--- app/src/main/java/com/example/tastezzip/data/repository/CafeteriaRepository.kt
+++ app/src/main/java/com/example/tastezzip/data/repository/CafeteriaRepository.kt
@@ -3,6 +3,7 @@ package com.example.tastezzip.data.repository
 import com.example.tastezzip.model.request.BookmarkCafeteriaRequestVo
 import com.example.tastezzip.model.request.SearchCafeteriaRequest
 import com.example.tastezzip.model.request.comment.get.GetCommentRequestVo
+import com.example.tastezzip.model.request.comment.post.CreateCommentRequestVo
 import com.example.tastezzip.model.response.cafeteria.SearchCafeteriaResponse
 import com.example.tastezzip.model.response.cafeteria.bookmark.BookmarkListResponse
 import com.example.tastezzip.model.response.cafeteria.detail.CafeteriaDetailResponse
@@ -14,4 +15,5 @@ interface CafeteriaRepository {
     suspend fun bookmarkCafeteria(request: BookmarkCafeteriaRequestVo)
     suspend fun getBookmarkList(): BookmarkListResponse
     suspend fun getComment(id: Long, request: GetCommentRequestVo): GetCommentResponse
+    suspend fun createComment(id: Long, request: CreateCommentRequestVo)
 }
--- app/src/main/java/com/example/tastezzip/data/repositoryImpl/CafeteriaRepositoryImpl.kt
+++ app/src/main/java/com/example/tastezzip/data/repositoryImpl/CafeteriaRepositoryImpl.kt
@@ -5,6 +5,7 @@ import com.example.tastezzip.data.repository.CafeteriaRepository
 import com.example.tastezzip.model.request.BookmarkCafeteriaRequestVo
 import com.example.tastezzip.model.request.SearchCafeteriaRequest
 import com.example.tastezzip.model.request.comment.get.GetCommentRequestVo
+import com.example.tastezzip.model.request.comment.post.CreateCommentRequestVo
 import com.example.tastezzip.model.response.cafeteria.SearchCafeteriaResponse
 import com.example.tastezzip.model.response.cafeteria.bookmark.BookmarkListResponse
 import com.example.tastezzip.model.response.cafeteria.detail.CafeteriaDetailResponse
@@ -35,5 +36,9 @@ class CafeteriaRepositoryImpl @Inject constructor(
 
     override suspend fun getComment(id: Long, request: GetCommentRequestVo): GetCommentResponse {
         return cafeteriaApi.getComment(id = id, pageable = request)
+    }
+
+    override suspend fun createComment(id: Long, request: CreateCommentRequestVo) {
+        return cafeteriaApi.createComment(id = id, request = request)
     }
 }
