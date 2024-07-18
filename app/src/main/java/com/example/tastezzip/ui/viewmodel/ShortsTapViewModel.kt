0614d4b71f5fe86459fb70773df4b3f4cd0a9bf5
dogmania <hj296914@gmail.com>
2024-07-19 01:31:39 +0900

    :sparkles: Feat: 쇼츠 탭 영상에도 영상 별점 기능 추가

--- app/src/main/java/com/example/tastezzip/ui/viewmodel/ShortsTapViewModel.kt
+++ app/src/main/java/com/example/tastezzip/ui/viewmodel/ShortsTapViewModel.kt
@@ -2,8 +2,12 @@ package com.example.tastezzip.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.tastezzip.application.LoadingManager
 import com.example.tastezzip.data.repository.VideoRepository
+import com.example.tastezzip.model.enums.LikeType
+import com.example.tastezzip.model.request.AddLikeVideoRequestVo
 import com.example.tastezzip.model.response.shorts.Feed
+import com.example.tastezzip.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -15,8 +19,9 @@ import javax.inject.Inject
 
 @HiltViewModel
 class ShortsTapViewModel @Inject constructor(
-    private val videoRepository: VideoRepository
-) : ViewModel() {
+    private val videoRepository: VideoRepository,
+    loadingManager: LoadingManager
+) : BaseViewModel(loadingManager) {
     private val _videoList: MutableStateFlow<List<Feed>> = MutableStateFlow(emptyList())
     val videoList = _videoList.asStateFlow()
 
@@ -24,10 +29,30 @@ class ShortsTapViewModel @Inject constructor(
         getFeedList()
     }
 
-    fun getFeedList() {
+    private fun getFeedList() {
         viewModelScope.launch(Dispatchers.Main) {
-            val response = videoRepository.getFeedList(5)
-            _videoList.update { response.feedList }
+            setLoading(true)
+            try {
+                val response = videoRepository.getFeedList(5)
+                _videoList.update { response.feedList }
+            } catch (e: Exception) {
+
+            } finally {
+                setLoading(false)
+            }
+        }
+    }
+
+    fun createVideoRating(videoId: Long, review: Double) {
+        viewModelScope.launch {
+            setLoading(true)
+            try {
+                videoRepository.addLikeVideo(request = AddLikeVideoRequestVo(videoId = videoId, type = LikeType.STAR, score = review))
+            } catch (e: Exception) {
+
+            } finally {
+                setLoading(false)
+            }
         }
     }
 }
